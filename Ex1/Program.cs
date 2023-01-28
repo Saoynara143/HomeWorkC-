cc5220ecf171172831774e900f5828622cb71a0c
Артем <ckripa2013@gmail.com>
2023-01-29 02:40:40 +0300

    Okey1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+ 
+            if(num1 > num2)
+            {
+                Console.WriteLine("Первое число больше второго числа");
+                Console.WriteLine($"Max={num1}; Min={num2}");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine("Второе число больше первого числа");
+                Console.WriteLine($"Max={num2}; Min={num1}");
+            }
+            else
+            {
+                Console.WriteLine("Эти числа равны");
+                Console.WriteLine($"{num1}={num2}");
+            }
+            Console.ReadKey();
+        }
+    }
