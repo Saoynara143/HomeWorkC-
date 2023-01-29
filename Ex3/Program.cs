b3d4e756eec7f0fbd2b6201208ff2d80a6bad3b0
Артем <ckripa2013@gmail.com>
2023-01-29 03:50:10 +0300

    Okay3

---
+++ Ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверьте является ли ваше число четным. Введите любое число:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+ 
+            if((num1 % 2) == 0)
+            {
+                Console.WriteLine($"{num1} -> Да, оно четное");
+            }     
+            else
+            {
+                Console.WriteLine($"{num1} -> Нет, оно нечетное");
+            }
+            Console.ReadKey();
+        }
+    }
